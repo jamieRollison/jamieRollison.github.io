aee4a16af210137386771824c7cf75bd48f1243e
jamieRollison <rollisonjamie15@gmail.com>
2023-03-29 23:42:49 -0500

    shamelessly base nav off of ellie

---
+++ src/components/Nav.svelte
@@ -0,0 +1,10 @@
+<main>
+    <nav class='h-14 flex justify-evenly text-center'>
+    <a href='/' class='text-light font-cookie pt-6 text-5xl'>Jamie</a>
+        <ul class='flex justify-evenly w-80 pt-10'>
+            <li class='text-light font-sans text-xl hover:animate-bounce'>about</li>
+            <li class='text-light font-sans text-xl hover:animate-bounce'>projects</li>
+            <li class='text-light font-sans text-xl hover:animate-bounce'>contact</li>
+        </ul>
+    </nav>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
     import "../app.css";
+    import Nav from "../components/Nav.svelte";
 </script>
 
-<main>
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class='bg-primary h-screen'>
+    <Nav/>
 </main>
