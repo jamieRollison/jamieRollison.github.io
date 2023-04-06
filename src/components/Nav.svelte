a6d67b2131d557cef807551da6153653025cfffb
jamieRollison <rollisonjamie15@gmail.com>
2023-04-05 20:07:29 -0500

    add pages

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,9 +2,11 @@
     <nav class='h-14 flex justify-evenly text-center'>
     <a href='/' class='text-light font-cookie pt-6 text-5xl'>Jamie</a>
         <ul class='flex justify-evenly w-80 pt-10'>
-            <li class='text-light font-sans text-xl hover:animate-bounce'>about</li>
-            <li class='text-light font-sans text-xl hover:animate-bounce'>projects</li>
-            <li class='text-light font-sans text-xl hover:animate-bounce'>contact</li>
+            <li class='text-light font-sans text-xl hover:animate-bounce'>
+                <a href="/about">about</a></li>
+            <li class='text-light font-sans text-xl hover:animate-bounce'><a href="/projects">projects</a></li>
+            <li class='text-light font-sans text-xl hover:animate-bounce'>
+                <a href="/contact">contact</a></li>
         </ul>
     </nav>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
     import Nav from "../components/Nav.svelte";
 </script>
 
-<main class='bg-primary h-screen'>
+<main class='h-screen'>
     <Nav/>
+    <p class='text-light text-sans text-5xl text-left pl-40 pt-20'>Welcome!</p>
+    <!-- <img class='h-80 pl-40' alt='me' src='/chicago picture.png'/> -->
 </main>
