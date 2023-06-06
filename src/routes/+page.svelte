61abda7873b368423227bb53ab12ce47af5077dd
jamieRollison <rollisonjamie15@gmail.com>
2023-06-05 17:59:54 -0700

    reduce to a releaseable state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,7 @@
 <script>
     import "../app.css";
-    import Nav from "../components/Nav.svelte";
 </script>
 
 <main class='h-screen'>
-    <Nav/>
-    <p class='text-light text-sans text-4xl text-center sm: p-20 pb-10'>Welcome!</p>
-    <div class='flex flex-row px-20 justify-evenly'>
-        <img class='h-80 w-60' alt='me' src='/chicago picture.png'/>
-        <p class='text-light text-sans text-2xl text-left w-60'>My name is Jamie and I'm a Computer Science major at UIUC</p>
-    </div>
+    <p class='text-md w-full text-center'>This site is a ✨work in progress!✨</p>
 </main>
--- src/routes/about/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import "../../app.css";
-  import Nav from "../../components/Nav.svelte";
-</script>
-
-<main>
-  <Nav/>
-  <p class='text-light text-sans text-5xl text-left pl-40 pt-20'>About</p>
-</main>
