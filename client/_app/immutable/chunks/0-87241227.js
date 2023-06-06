bf24c8ca26ec2d588f7e0f2045d0d9619c6fbd3a
jamieRollison <rollisonjamie15@gmail.com>
2023-06-05 18:58:05 -0700

    remove files from mistake

--- client/_app/immutable/chunks/0-87241227.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-3df77e27.js";export{t as component};
--- client/_app/immutable/chunks/3-ebdd57e3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/contact/_page.svelte-09c7704c.js";export{t as component};
