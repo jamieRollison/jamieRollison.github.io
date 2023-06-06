ab040e596578140d26532461e06a05d1db5d2058
jamieRollison <rollisonjamie15@gmail.com>
2023-06-05 18:52:28 -0700

    yeah

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,9 +3,6 @@
 		<a href="/" class="text-light font-cookie pt-6 text-5xl">Jamie</a>
 		<ul class="flex justify-evenly w-80 pt-10">
 			<li class="text-light font-sans text-xl hover:animate-bounce">
-				<a href="/about">about</a>
-			</li>
-			<li class="text-light font-sans text-xl hover:animate-bounce">
 				<a href="/projects">projects</a>
 			</li>
 			<li class="text-light font-sans text-xl hover:animate-bounce">
